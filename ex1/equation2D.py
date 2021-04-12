0afaa9d68ae17963df1950fe983a96c26f340d4c
foojanbabaeeian <79134873+foojanbabaeeian@users.noreply.github.com>
2021-04-12 20:31:47 +0430

    Add files via upload

---
+++ ex1/equation2D.py
@@ -0,0 +1,12 @@
+def equation2D (a,b,c):
+    '''int :::> float
+this function gets a,b,c of a quadratic equation formula , and calculates the roots . if there is none :::> none ,and if there is one :::> x1=x2=the number'''
+    delta=(b**2)-(4*a*c)
+    x1=(-b+(delta**0.5))/2*a
+    x2=(-b-(delta**0.5))/2*a
+    if delta==0:
+        print ('x1=x2={}'.format(x1))
+    elif delta>0:
+        print ('x1={} and x2={}'.format(x1,x2))
+    else:
+        print ('none')
