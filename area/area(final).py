19f2da8e1d9dd9ee5845db6f6e389cce8959664b
foojanbabaeeian <79134873+foojanbabaeeian@users.noreply.github.com>
2021-04-12 21:38:15 +0430

    Delete area directory

--- area/area(final).py
+++
@@ -1,5 +0,0 @@
-def area(x, h):
-	return 0.5 * x * h
-x=input ('what is the edge of the triangle?')
-h=input ('what is the height of the triangle?')
-print(area(int(x), int(h)))
--- area/area.py
+++
@@ -1,4 +0,0 @@
-x=input ('what is the edge of the triangle?')
-h=input ('what is the height of the triangle?')
-def area(x,h):
-  return (x*h)/2
