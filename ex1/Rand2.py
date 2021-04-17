5280f541a261f13e39e6632b31bbd3ec2d846ef5
foojanbabaeeian <79134873+foojanbabaeeian@users.noreply.github.com>
2021-04-17 20:01:19 +0430

    Update Rand2.py

--- ex1/Rand2.py
+++ ex1/Rand2.py
@@ -2,19 +2,30 @@ import random
 #E stands for EVEN numbers
 #O stands for ODD numbers
 #ex :::> bEO is the amount of even numbers between an even and odd number
-def Rand2(num1,num2):
-    '''int :::> int
+    '''int :::> none
 this function gets two numbers and gives an even number randomly between them'''
-    bEO=(num2-(num1+1))/2
-    bEE=(num2-num1-2)/2
-    bOO=(num2-num1)/2
-    start=1
-    if (num1%2==0 and num2%2==1) or (num2%2==0 and num1%2==1):
-        nEO=random.randint (1,bEO)
-        print (num1+2*nEO)
-    elif (num1%2==0 and num2%2==0):
-        nEE=random.randint (1,bEE)
-        print(num1+2*nEE)
+
+def Rand2(a,b):
+    if a==b:
+        return None
+    elif a>b:
+        max = a
+        min = b
     else:
-        nOO=random.randint (1,bOO)
-        print (num1+2*nOO-1)
+        max = b
+        min = a
+    if (max - min == 1) or (max-min == 2 and max%2==0):
+        return None
+    else:
+        bEO=(max-(min+1))/2
+        bEE=(max-min-2)/2
+        bOO=(max-min)/2
+        if (min%2==0 and max%2==1) or (max%2==0 and min%2==1):
+            nEO=random.randint (1,bEO)
+            return (min+2*nEO)
+        elif (min%2==0 and max%2==0):
+            nEE=random.randint (1,bEE)
+            return(min+2*nEE)
+        else:
+            nOO=random.randint (1,bOO)
+            return (min+2*nOO-1)
